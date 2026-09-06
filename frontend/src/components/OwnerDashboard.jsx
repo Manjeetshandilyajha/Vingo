f81c269f1fa941c85ec46c3b317c603df91612c6
Manjeet Kumar Jha <manjeetshandilya16@gmail.com>
2026-09-06 18:08:09 +0530

    Ui update

--- frontend/src/components/OwnerDashboard.jsx
+++ frontend/src/components/OwnerDashboard.jsx
@@ -63,7 +63,7 @@ const OwnerDashboard = () => {
             </div>
           }
 
-          {myShopData.items.length > 0 && <div className="flex flex-col items-center gap-4 w-full max-w-3xl">
+          {myShopData.items.length > 0 && <div className="flex flex-col items-center gap-4 w-full max-w-3xl mb-20">
             {myShopData.items.map((item, index) => (
               <OwnerItemCard data={item} key={index} />
             ))}
