587931cd112cecbd54acda8d009d9baab6d0a06c
Manjeet Kumar Jha <manjeetshandilya16@gmail.com>
2026-09-06 10:07:45 +0530

    Update server URL to production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,8 @@ import { useEffect } from 'react'
 import { io } from 'socket.io-client'
 import { setSocket } from './redux/userSlice'
 
-export const serverUrl = "http://localhost:4000"
+// export const serverUrl = "http://localhost:4000"
+export const serverUrl = "https://vingo-backend-utwq.onrender.com"
 const App = () => {
   const { userData } = useSelector(state => state.user)
   const dispatch=useDispatch()
